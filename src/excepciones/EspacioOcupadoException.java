815960f99dcc1686217af24d677919b5444ef94d
Luna <carlosguzmanluna8@gmail.com>
2026-08-21 17:24:46 -0600

    orden de archivos
    se corrigio el orden de los archivos y se borro util

--- src/excepciones/EspacioOcupadoException.java
+++ src/excepciones/EspacioOcupadoException.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package util;
+package excepciones;
 
 /**
  *
  * @author norki
  */
-public class Estructuras_dinamicas {
+public class EspacioOcupadoException {
     
 }
