1a5c499a18ea1d4fe4392427f957023c926c56d8
Luna <carlosguzmanluna8@gmail.com>
2026-08-21 21:20:52 -0600

    correcion

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -1,37 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author norki
- */
 public abstract class Persona {
- private final String identificacion;
+    private final String identificacion;
     private String nombreCompleto;
-    private String telefono;  
-    
-protected Persona (String identificacion, String nombreCompleto, String telefono) {
+    private String telefono;
+
+    protected Persona(String identificacion, String nombreCompleto, String telefono) {
         this.identificacion = identificacion;
         this.nombreCompleto = nombreCompleto;
         this.telefono = telefono;
     }
+
     public String getIdentificacion() {
         return identificacion;
     }
+
     public String getNombreCompleto() {
         return nombreCompleto;
     }
- 
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
     public String getTelefono() {
         return telefono;
     }
-    public void SetTelefono(String Telefono){
-        this.telefono=telefono;
-    } 
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
-
-
- 
