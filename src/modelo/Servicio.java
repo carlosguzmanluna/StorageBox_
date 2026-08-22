dcfa7e870778f8fc7c0602b8c269a7f317091aab
Mari <mafloresur@est.utn.ac.cr>
2026-08-21 19:05:19 -0600

    Se programo el enum de estadoContrato y PuestoEmpleado

--- src/modelo/Servicio.java
+++ src/modelo/Servicio.java
@@ -9,5 +9,44 @@ package modelo;
  * @author norki
  */
 public class Servicio {
-    
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+public Servicio(int codigo, String nombre, String descripcion, double precio ){
+this.codigo = codigo;
+this.nombre = nombre;
+this.descripcion = descripcion;
+this. precio = precio;
+
+}
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+public String toString(){
+    return codigo + "-"+ nombre;
+}
 }
