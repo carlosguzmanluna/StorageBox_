871ab1c98e1118ad14995e8c943a4b5a2f2c1770
Mari <mafloresur@est.utn.ac.cr>
2026-08-21 21:03:20 -0600

    Se realizo enum EstadoEspacio

--- src/modelo/EstadoEspacio.java
+++ src/modelo/EstadoEspacio.java
@@ -9,5 +9,6 @@ package modelo;
  * @author norki
  */
 public enum EstadoEspacio {
-    
+    Disponible,
+    Ocupado;
 }
--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -24,18 +24,13 @@ protected Persona (String identificacion, String nombreCompleto, String telefono
     public String getNombreCompleto() {
         return nombreCompleto;
     }
-    public void setNombreCompleto(String nombreCompleto) {
-        this.nombreCompleto = nombreCompleto;
-    }
+ 
     public String getTelefono() {
         return telefono;
     }
     public String SetTelefono(){
         return telefono;
-    }
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+    } 
 }
 
 
